5dfd42961bd13218d5aca4c003e21cd49119ea73
aurimasrim <aurimas1rimkus@gmail.com>
2023-04-01 00:19:19 +0300

    Fix question value parsing.

--- src/Parser/QuestionParser.php
+++ src/Parser/QuestionParser.php
@@ -19,46 +19,58 @@ class QuestionParser
         $questions = $crawler->filter('div.ui.card')
             ->each(
                 function (Crawler $questionCard): ?Question {
-                    $header = $questionCard->filter('.header')->text();
-                    if (str_contains($header, 'Congratulation')) {
-                        return null;
-                    }
-                    preg_match('/.*\s([A-Za-z]+)$/', $header, $matches);
-                    if (!isset($matches[1])) {
-                        throw new \InvalidArgumentException('Invalid header: ' . $header . ' on file ' . '');
-                    }
-                    $category = $matches[1];
-
-                    // todo: wrong!
-                    $question = $questionCard->filter('.content > p')->text();
-
-                    $answers = $questionCard->filter('h3:contains("choices were")')
-                        ->ancestors()
-                        ->eq(0)
-                        ->filter('ul > li')
-                        ->each(
-                            static fn (Crawler $answer): string => $answer->text(),
-                        );
-//                    var_dump($answers);
-
-                    // todo: wrong!
-                    $correctHeader = $questionCard->filter('h3:contains("Correct")');
-                    if ($correctHeader->count() === 0) {
-                        $correctAnswers = [];
-                    } else {
-                        $correctAnswers = $correctHeader
-                            ->ancestors()
-                            ->eq(0)
-                            ->filter('ul > li')
-                            ->each(
-                                static fn (Crawler $answer): string => $answer->text(),
-                            );
-                    }
-
-                    return new Question($question, $answers, $correctAnswers ,'', $category);
+                    return $this->parseQuestionCard($questionCard);
                 },
             );
 
         return array_filter($questions);
+    }
+
+    private function parseQuestionCard(Crawler $questionCard): ?Question
+    {
+        $header = $questionCard->filter('.header');
+        $headerText = $header->text();
+        if (str_contains($headerText, 'Congratulation')) {
+            return null;
+        }
+        preg_match('/.*\s([A-Za-z]+)$/', $headerText, $matches);
+        if (!isset($matches[1])) {
+            throw new \InvalidArgumentException('Invalid header: ' . $headerText);
+        }
+        $category = $matches[1];
+
+        $start = $questionCard->filter('.content > p')->first();
+        $question = $start->text(null, false);
+        foreach ($start->nextAll() as $element) {
+            $elementCrawler = new Crawler($element);
+            if (str_contains($element->textContent, 'Correct') || str_contains($element->textContent, 'Wrong')) {
+                break;
+            }
+            $question .= "\n" .$elementCrawler->text(null, false);
+        }
+
+        $answers = $questionCard->filter('h3:contains("choices were")')
+            ->ancestors()
+            ->eq(0)
+            ->filter('ul > li')
+            ->each(
+                static fn (Crawler $answer): string => $answer->text(),
+            );
+
+        // todo: wrong!
+        $correctHeader = $questionCard->filter('h3:contains("Correct")');
+        if ($correctHeader->count() === 0) {
+            $correctAnswers = [];
+        } else {
+            $correctAnswers = $correctHeader
+                ->ancestors()
+                ->eq(0)
+                ->filter('ul > li')
+                ->each(
+                    static fn (Crawler $answer): string => $answer->text(),
+                );
+        }
+
+        return new Question($question, $answers, $correctAnswers ,'', $category);
     }
 }
